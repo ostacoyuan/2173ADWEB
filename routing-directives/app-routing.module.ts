973b04b0ca16dd14c24f9585a508d73b4257bda6
ostacoyuan <123538413+ostacoyuan@users.noreply.github.com>
2024-02-02 13:53:10 +0800

    Add files via upload

---
+++ routing-directives/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DashboadComponent } from './dashboad/dashboad.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DirectivesComponent } from './directives/directives.component';
+
+const routes: Routes = [{path:'home', component:HomeComponent}, {path:'about', component:AboutComponent}, {path:'dashboard', component:DashboadComponent},
+{path:'data-binding', component:DataBindingComponent},{path:'directives', component:DirectivesComponent} ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
