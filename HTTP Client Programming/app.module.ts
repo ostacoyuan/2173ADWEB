db4bbbdc9a506b69b0820edc80c806428e893eee
ostacoyuan <123538413+ostacoyuan@users.noreply.github.com>
2024-02-29 09:31:07 +0800

    Add files via upload

---
+++ HTTP Client Programming/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpclientService } from './httpclient.service';
+import { HttpclientComponent } from './httpclient/httpclient.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HttpclientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [HttpclientService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
